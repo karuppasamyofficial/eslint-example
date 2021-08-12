fbacb93fcdab7f5866eb6f0d430513c6dd6b1f3f
Karuppasamy Bala <karuppasamy.bala@aspiresys.com>
2021-08-12 12:11:25 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-    welcome 
+    welcome to Aspire
         </p>
         <a
           className="App-link"
