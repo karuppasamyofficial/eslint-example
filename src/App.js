b7a82f99dbd4d9625e1a47e66301cd3dce06f252
Karuppasamy Bala <karuppasamy.bala@aspiresys.com>
2021-08-16 19:48:57 +0530

    completed crud

--- src/App.js
+++ src/App.js
@@ -1,28 +1,132 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Grid from '@material-ui/core/Grid';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
+import UserForm from './UserForm';
 
-// function addSum() {
-//   console.log('nfvn');
-//   return '';
-// }
+const initialData = [{
+  id: '1',
+  name: 'sekar',
+  emailId: 'sekar@gmail.com',
+  designation: 'Developer',
+
+},
+{
+  id: '2',
+  name: 'karuppasamy',
+  emailId: 'karuppasamy@gmail.com',
+  designation: 'Consultant',
+
+},
+];
 function App() {
+  const [open, setOpen] = React.useState(false);
+  const [currentUser, setCurrentUser] = React.useState(null);
+  const [userData, setUserData] = React.useState(initialData);
+  const [flow, setFlow] = React.useState(null);
+  const handleClickOpen = () => {
+    setFlow('add');
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setFlow(null);
+  };
+
+  const onClickEdit = (data) => {
+    setFlow('edit');
+    setOpen(true);
+    setCurrentUser(data);
+  };
+
+  const onSubmitForm = (userInfo) => {
+    if (flow === 'add') {
+      const newUserList = [...userData, userInfo];
+      setUserData(newUserList);
+    } else {
+      const updateUseList = userData;
+      const foundIndex = updateUseList.findIndex((element) => element.id === userInfo.id);
+      updateUseList.splice(foundIndex, 1, userInfo);
+      setUserData(updateUseList);
+    }
+
+    handleClose();
+  };
+
+  const deleteUserItem = (userInfo) => {
+    const newArr = userData.filter((element) => element.id !== userInfo.id);
+    setUserData(newArr);
+  };
+
+  console.log('onSubmitForm', userData);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>removed the file extensions</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="topbar">
+        <Button className="add-btn" data-testid="add-user" onClick={handleClickOpen}>
+          Add user
+        </Button>
+      </div>
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title"><UserForm currentUser={currentUser} onSubmitForm={onSubmitForm} handleClose={handleClose} /></DialogTitle>
+        <DialogContent />
+
+      </Dialog>
+      <Grid>
+
+        <TableContainer data-testid="user-table">
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Email Id</TableCell>
+                <TableCell>Designation</TableCell>
+                <TableCell>Action</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {userData.map((data) => (
+                <TableRow key={data.id}>
+                  <TableCell>
+                    {data.name}
+                  </TableCell>
+                  <TableCell>
+                    {data.emailId}
+                  </TableCell>
+
+                  <TableCell>
+                    {data.designation}
+                  </TableCell>
+                  <div>
+                    <Button variant="outlined" color="primary" onClick={() => { onClickEdit(data); }}>
+                      edit
+                    </Button>
+                    <Button variant="outlined" color="primary" onClick={() => { deleteUserItem(data); }}>
+                      delete
+                    </Button>
+                  </div>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+
     </div>
   );
 }
-
 export default App;
---
+++ src/UserForm.js
@@ -0,0 +1,141 @@
+import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import { FormControl } from '@material-ui/core';
+import InputLabel from '@material-ui/core/InputLabel';
+
+class UserForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    const { currentUser } = this.props;
+    console.log('userform edit field', currentUser);
+    if (currentUser != null) {
+      this.setState({
+        id: currentUser.id,
+        name: currentUser.name,
+        emailId: currentUser.emailId,
+        designation: currentUser.designation,
+      });
+    } else {
+      this.setState({
+        id: '', name: '', emailId: '', designation: '',
+      });
+    }
+  }
+/* eslint-disable */
+
+  onSubmitForm = () => {
+      const { onSubmitForm } = this.props;
+      const {
+        id, name, emailId, designation,
+      } = this.state;
+      const userObject = {
+        id,
+        name,
+        emailId,
+        designation,
+      };
+      onSubmitForm(userObject);
+    }
+    /* eslint-enable */
+
+  render() {
+    const {
+      id, name, emailId, designation,
+    } = this.state; const { handleClose } = this.props;
+    return (
+      <form>
+        <Grid container>
+          <Grid container className="container-btm">
+            <Grid md={6}>
+              <FormControl rolLabel><InputLabel>Id</InputLabel></FormControl>
+
+            </Grid>
+            <Grid md={6}>
+              <TextField
+                data-testid="id"
+                value={id}
+                fullWidth
+                variant="outlined"
+                onChange={(event) => {
+                  this.setState({ id: event.target.value });
+                }}
+              />
+
+            </Grid>
+          </Grid>
+          <Grid container className="container-btm">
+            <Grid md={6}>
+              <FormControl rolLabel><InputLabel>Name</InputLabel></FormControl>
+
+            </Grid>
+            <Grid md={6}>
+              <TextField
+                data-testid="name"
+                value={name}
+                fullWidth
+                variant="outlined"
+                onChange={(event) => {
+                  this.setState({ name: event.target.value });
+                }}
+              />
+
+            </Grid>
+          </Grid>
+          <Grid container className="container-btm">
+            <Grid md={6}>
+              <FormControl rolLabel><InputLabel>Email Id</InputLabel></FormControl>
+
+            </Grid>
+            <Grid md={6}>
+              <TextField
+                data-testid="emailId"
+                value={emailId}
+                fullWidth
+                variant="outlined"
+                onChange={(event) => {
+                  this.setState({ emailId: event.target.value });
+                }}
+              />
+
+            </Grid>
+          </Grid>
+          <Grid container className="container-btm">
+            <Grid md={6}>
+              <FormControl rolLabel><InputLabel>Designation</InputLabel></FormControl>
+
+            </Grid>
+            <Grid md={6}>
+              <TextField
+                data-testid="designation"
+                value={designation}
+                fullWidth
+                variant="outlined"
+                onChange={(event) => {
+                  this.setState({ designation: event.target.value });
+                }}
+              />
+
+            </Grid>
+          </Grid>
+
+          <div>
+            <Button onClick={this.onSubmitForm} className="submit-btn">
+              submit
+            </Button>
+            <Button onClick={handleClose} className="cancel-btn">
+              cancel
+            </Button>
+          </div>
+        </Grid>
+      </form>
+    );
+  }
+}
+
+export default UserForm;
